67fdd1a279ff3e645d989438b8a634c234c4b0c6
Kalani09 <kalanichandrasena@gmail.com>
2025-10-26 11:56:24 +0530

    Added routes for module and quiz pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
-import { Routes } from '@angular/router';
+import { Router, RouterModule, Routes } from '@angular/router';
+import { ModulePageComponent } from './components/module-page/module-page.component';
+import { NgModule } from '@angular/core';
+import { QuizPageComponent } from './components/quiz-page/quiz-page.component';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+    {path:'module', component: ModulePageComponent},
+    { path: 'quiz', component: QuizPageComponent },
+];
+
+@NgModule({
+    imports:[RouterModule.forRoot(routes)],
+    exports:[RouterModule]
+})
+
+export class AppRoutingModule {}
